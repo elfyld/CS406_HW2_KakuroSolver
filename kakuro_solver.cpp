f84f7f801662bba2f88b9fe36c1664b0dc21e3a6
elfyld <elifyildirir@sabanciuniv.edu>
2023-05-07 00:21:24 +0000

    Sequential solution

--- kakuro_solver.cpp
+++ kakuro_solver.cpp
@@ -199,6 +199,7 @@ vector<sum> get_sums(int** matrix, int m, int n){
 	  COORD START = COORD(i, j+1); 
 	  COORD END = find_end(matrix, m, n, i, j, d_right);
 	  sum _sum = sum(START, END, hint, d_right);
+      _sum.print_sum(); 
 	  sums.push_back(_sum);
 	}
 
@@ -232,21 +233,121 @@ vector<sum> get_sums(int** matrix, int m, int n){
   return sums;
 }
 
+pair<int, int> find_unassigned_location(int** sol_mat, int m, int n) {
+  for (int row = 0; row < m; row++) {
+    for (int col = 0; col < n; col++) {
+      if (sol_mat[row][col] == -2) {
+        return make_pair(row, col);
+      }
+    }
+  }
+  return make_pair(-1, -1);
+}
 
-void solution(int** mat, int** sol_mat, vector<sum> sums, int m, int n){
+bool is_safe(int** sol_mat, int row, int col, int num, const vector<sum>& sums, int m ,int n) {
+  
+  
+  // Check if the number is valid according to the sum hints
+  for (int j = 0; j < m; j++) {
+    if (sol_mat[row][j] == num) {
+      return false;
+    }
+  }
+   for (int j = 0; j < n; j++) {
+    if (sol_mat[j][col] == num) {
+      return false;
+    }
+  }
+
+  for (const sum& s : sums) {
+    if ((s.dir == d_down && col == s.start.second && row >= s.start.first && row < s.end.first) ||
+        (s.dir == d_right && row == s.start.first && col >= s.start.second && col < s.end.second)) {
+      
+      int sum_value = num;
+      bool last_cell = true;
+      if (s.dir == d_down) {
+        for (int i = s.start.first; i < s.end.first; i++) {
+          if (i != row && sol_mat[i][col] != -2) {
+            sum_value += sol_mat[i][col];
+          }
+          if (sol_mat[i][col] == -2 && i != row) {
+            last_cell = false;
+          }
+        }
+      } else {
+        for (int j = s.start.second; j < s.end.second; j++) {
+          if (j != col && sol_mat[row][j] != -2) {
+            sum_value += sol_mat[row][j];
+          }
+          if (sol_mat[row][j] == -2 && j != col) { //if I am not at the end of the column and there exist -2 I am not in last cell
+            last_cell = false;
+          }
+        }
+      }
+
+      if (sum_value > s.hint || (last_cell && sum_value != s.hint)) {//if sum value satisifes sum constraint then the numbers are correct 
+        return false;
+      }
+    }
+  }
+
+  return true;
+}
+
+
+
+// Rename validNumberCheck to solve_kakuro_helper
+bool validNumberCheck(int** sol_mat, int m, int n, const vector<sum>& sums) {
+  pair<int, int> unassigned_location = find_unassigned_location(sol_mat, m, n);
+  if (unassigned_location.first == -1) {
+    return true;
+  }
+  
+  int row = unassigned_location.first;
+  int col = unassigned_location.second;
+
+  for (int num = 1; num <= 9; num++) {
+    if (is_safe(sol_mat, row, col, num, sums,m,n) ) {
+      sol_mat[row][col] = num;
+       cout << "Trying number " << num << " at row " << row << " and col " << col << endl;
+
+      if (validNumberCheck(sol_mat, m, n, sums)) {
+        return true;
+      }
+
+      sol_mat[row][col] = -2;
+    }
+  }
+
+  return false;
+}
+
+
+
+
+  
+  
+
+
+ 
+
+
+
+
+bool solution(int** mat, int** &sol_mat, vector<sum> sums, int m, int n){
 
   //TO DO: Write the solution
   //You can use any algorithm and data type
   //Write your solution to file in main function using sol_to_mat() after solving it
+   return validNumberCheck(sol_mat, m, n, sums);
   
 }
 
 int main(int argc, char** argv){
   
-  std::string filename(argv[1]);
+   std::string filename(argv[1]);
   std::ifstream file;
   file.open(filename.c_str());
-
   int m, n;
   file >> m;
   file >> n;
@@ -260,7 +361,9 @@ int main(int argc, char** argv){
   print_one_matrix(sol_mat, m, n);
   
   vector<sum> sums = get_sums(mat, m, n);
-  solution(mat, sol_mat, sums, m, n);
+
+  cout << "Solution: " <<solution(mat, sol_mat, sums, m, n) << endl;
+
   print_one_matrix(sol_mat, m, n);
   sol_to_file(mat, sol_mat, m, n, "solution.kakuro");
   
